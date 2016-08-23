ada9e30e953e0e43d27bd85e36217a6b03dc3919
Jd Garza <jd@leadpropeller.com>
2016-08-23 11:37:01 -0500

    First commit of personal website

---
+++ resources/assets/js/components/home.vue
@@ -0,0 +1,21 @@
+<style>
+    body {
+        position: relative;
+    }
+</style>
+
+<template>
+    <div id="home-navbar">
+        <ul class="nav nav-tabs" role="tablist">
+            
+        </ul>
+    </div>
+</template>
+
+<script>
+    require('bootstrap');
+
+    export default {
+
+    }
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Jd Garza - Software Developer</title>
+    </head>
+    <body data-spy="scroll" data-target="#home-navbar">
+        <home></home>
+        <script src="js/app.js"></script>
+    </body>
+</html>
